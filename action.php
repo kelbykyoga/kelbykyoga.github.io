5abf7cc43b909684452383715e8a8e5cb2dd5485
kelbykyoga <60272700+kelbykyoga@users.noreply.github.com>
2020-01-29 14:42:20 -0700

    Add files via upload

--- action.php
+++ action.php
@@ -1,16 +1,62 @@
 <?php
-  	$name = $_POST['name'];
-  	$visitor_email = $_POST['email'];
- 	$message = $_POST['comment'];
-	$email_from = 'kelbykyoga@kelbykyoga.com';
-	$email_subject = "Contact from Website";
-	$email_body = "You have received a new message from the user $name.\n".
-					+ "Email $visitor_email.\n".
-                    + "Comments: $message".;
- 	$to = "jacegummersall@gmail.com";
- 	//$to = "kelbykyoga@gmail.com";
-  	$headers = "From: $email_from \r\n";
-  	$headers .= "Reply-To: $visitor_email \r\n";
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['comment'];
 
-    mail($to,$email_subject,$email_body,$headers);
-?>
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'kelbykyoga@kelbykyoga.github.io';//<== update the email address
+$email_subject = "New Contact Message!";
+$email_body = "You have received a new message from $name.\n".
+	"Email: $visitor_email"
+    "Comments:\n $message".
+    
+$to = "jacegummersall@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect back to contact for page.
+header('Location: index.html/#contact');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
